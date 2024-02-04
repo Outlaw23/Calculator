034e2763958a0d2e872d52894ab5c4048df710b5
Outlaw23 <127243391+Outlaw23@users.noreply.github.com>
2024-02-04 02:23:14 +0100

    version 1.0.2
    I put the left and right numbers in an array and change left and right number in to the array

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Calculator {
 
-	private static final int leftHandNumber = 0;
+	private static final int LeftHandNumber = 0;
 	private static final int rightHandNumber = 1;
 	private static final int operator = 2;
 
-	int mode = leftHandNumber;
-	double LeftHandNumber = 0, RightHandNumber = 0;
+	int mode = LeftHandNumber;
+	double[] numbers = {0, 0};
 	String message = "";
 
 
@@ -28,28 +28,28 @@ public class Calculator {
 
 	public void plusOperator() {
 
-		System.out.println(LeftHandNumber + " + " + RightHandNumber + " = "
-				+ (LeftHandNumber + RightHandNumber));
+		System.out.println(numbers[0] + " + " + numbers[1] + " = "
+				+ (numbers[0] + numbers[1]));
 	}
 
 	public void minusOperator() {
-		System.out.println(LeftHandNumber + " - " + RightHandNumber + " = "
-				+ (LeftHandNumber - RightHandNumber));
+		System.out.println(numbers[0] + " - " + numbers[1] + " = "
+				+ (numbers[0] - numbers[1]));
 	}
 
 	public void divideOperator() {
-		System.out.println(LeftHandNumber + " / " + RightHandNumber + " = "
-				+ (LeftHandNumber / RightHandNumber));
+		System.out.println(numbers[0] + " / " + numbers[1] + " = "
+				+ (numbers[0] / numbers[1]));
 	}
 
 	public void duplicateOperator() {
-		System.out.println(LeftHandNumber + " * " + RightHandNumber + " = "
-				+ (LeftHandNumber * RightHandNumber));
+		System.out.println(numbers[0] + " * " + numbers[1] + " = "
+				+ (numbers[0] * numbers[1]));
 	}
 
 	public void percentageOperator() {
-		System.out.println(LeftHandNumber + " % " + RightHandNumber + " = "
-				+ (LeftHandNumber % RightHandNumber));
+		System.out.println(numbers[0] + " % " + numbers[1] + " = "
+				+ (numbers[0] % numbers[1]));
 	}
 
 
@@ -64,9 +64,9 @@ public class Calculator {
 			}
 
 
-			if (mode == leftHandNumber) {
+			if (mode == numbers[0]) {
 				System.out.println("Please type the first number.");
-			} else if (mode == rightHandNumber) {
+			} else if (mode == numbers[1]) {
 				System.out.println("Please type the second number.");
 			} else {
 				System.out.println("Please type the operator +, -, /, *, %");
@@ -75,16 +75,16 @@ public class Calculator {
 
 			message = scanner.nextLine();
 
-			if (mode == leftHandNumber) {
+			if (mode == numbers[0]) {
 				try {
-					LeftHandNumber = Double.parseDouble(message);
+					numbers[0] = Double.parseDouble(message);
 				} catch (NumberFormatException numberFormatException) {
 					System.out.println("invald operator: " + message);
 					continue;
 				}
-			} else if (mode == rightHandNumber) {
+			} else if (mode == numbers[1]) {
 				try {
-					RightHandNumber = Double.parseDouble(message);
+					numbers[1] = Double.parseDouble(message);
 				} catch (NumberFormatException numberFormatException) {
 					System.out.println("invald operator: " + message);
 					continue;
